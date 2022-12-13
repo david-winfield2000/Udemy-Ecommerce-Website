dfe341ccfd9ae38dff6666b3fde6e329f62e4eba
David Ryder <67267592+david-ryder@users.noreply.github.com>
2022-12-13 15:02:08 -0500

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
