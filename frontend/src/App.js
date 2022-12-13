8590453b8be354ebe13b9666bce23df7d7025351
David Ryder <67267592+david-ryder@users.noreply.github.com>
2022-12-13 16:40:01 -0500

    current progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import React from 'react';
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h1>Hello</h1>
+      <main className='py-5'>
+        <Container>
+          <h1>Hello</h1>
+        </Container>
       </main>
       <Footer />
     </div>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Navbar, Nav, Container, Row, NavDropdown, Form, Button } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div>
-        <header>Header</header>
-    </div>
+    <header>
+      <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/">eCommerce Site</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ml-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i> Login</Nav.Link>
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          {/* <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
   )
 }
 
